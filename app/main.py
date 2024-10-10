3bdab33325f95232e00a0eb3f417cf8bf6addfa8
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-10-10 11:09:05 +0700

    I

--- app/main.py
+++ app/main.py
@@ -22,3 +22,7 @@ app.include_router(match.router, prefix='/match')
 @app.get("/")
 async def root():
     return {"message": "Hello Big Buddy!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
